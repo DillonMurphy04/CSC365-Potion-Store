d704cb5f65bc2e4a9328d58d3fad985af659ce9b
Dillon Murphy <ddmurphy@calpoly.edu>
2023-10-04 11:51:37 -0700

    finished barrels, moved cart update

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -26,19 +26,32 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 
     red_ml = 0
     red_price = 0
+    green_ml = 0
+    green_price = 0
+    blue_ml = 0
+    blue_price = 0
     for barrel in barrels_delivered:
         if barrel.sku == "SMALL_RED_BARREL":
-            red_ml += barrel.ml_per_barrel # * barrel.quantity ??
-            red_price += barrel.price # * barrel.quantity ??
+            red_ml += barrel.ml_per_barrel
+            red_price += barrel.price
             break
+        if barrel.sku == 'SMALL_GREEN_BARREL':
+            green_ml += barrel.ml_per_barrel
+            green_price += barrel.price
+        if barrel.sku == 'SMALL_BLUE_BARREL':
+            blue_ml += barrel.ml_per_barrel
+            blue_price += barrel.price
 
     with db.engine.begin() as connection:
         connection.execute(
             sqlalchemy.text(
                 "UPDATE global_inventory "
-                "SET num_red_ml = num_red_ml + :red_ml, gold = gold - :red_price"
+                "SET num_red_ml = num_red_ml + :red_ml, "
+                "num_green_ml = num_green_ml + :green_ml, "
+                "num_blue_ml = num_blue_ml + :blue_ml, "
+                "gold = gold - :red_price - :green_price - :blue_price"
             )
-            .params(red_ml=red_ml, red_price=red_price)
+            .params(red_ml=red_ml, green_ml=green_ml, blue_ml=blue_ml, red_price=red_price, green_price=green_price, blue_price=blue_price)
         )
 
     return "OK"
@@ -50,20 +63,42 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print(wholesale_catalog)
 
     with db.engine.begin() as connection:
-        red_potions = connection.execute(sqlalchemy.text("SELECT num_red_potions, gold FROM global_inventory")).first()
+        potions = connection.execute(
+            sqlalchemy.text(
+                "SELECT num_red_potions, num_green_potions, num_blue_potions, gold "
+                "FROM global_inventory"
+                )
+                ).first()
 
-    red_price = 0
+    potion_info = {
+        "SMALL_RED_BARREL": 0,
+        "SMALL_GREEN_BARREL": 0,
+        "SMALL_BLUE_BARREL": 0,
+    }
+
     for barrel in wholesale_catalog:
         if barrel.sku == "SMALL_RED_BARREL":
-            red_price += barrel.price
-            break
+            potion_info["SMALL_RED_BARREL"] += barrel.price
+        elif barrel.sku == 'SMALL_GREEN_BARREL':
+            potion_info["SMALL_GREEN_BARREL"] += barrel.price
+        elif barrel.sku == 'SMALL_BLUE_BARREL':
+            potion_info["SMALL_BLUE_BARREL"] += barrel.price
     
-    if (red_potions.num_red_potions < 10 and red_potions.gold >= red_price):
-        return [
-            {
-                "sku": "SMALL_RED_BARREL",
-                "quantity": 1,
-            }
-        ]
-    else:
-        return []
+    purchase_plan = []
+    gold = potions.gold
+
+    potions_sorted = [
+        (potions.num_red_potions, "SMALL_RED_BARREL"),
+        (potions.num_blue_potions, "SMALL_BLUE_BARREL"),
+        (potions.num_green_potions, "SMALL_GREEN_BARREL"),
+    ]
+
+    potions_sorted.sort()
+
+    for quantity, name in potions_sorted:
+        price = potion_info[name]
+        if quantity < 10 and gold >= price:
+            purchase_plan.append({"sku": name, "quantity": 1})
+            gold -= price
+    
+    return purchase_plan
--- src/api/carts.py
+++ src/api/carts.py
@@ -49,14 +49,6 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     print(f"{cart_id}: {item_sku}")
     print(f"{cart_id}: {cart_item}")
 
-    with db.engine.begin() as connection:
-        connection.execute(
-            sqlalchemy.text(
-                "UPDATE global_inventory "
-                "SET num_red_potions = num_red_potions - :bottles, gold = gold + :gold"
-            )
-            .params(bottles=cart_item.quantity, gold=cart_item.quantity * 50)
-        )
     cart_items[cart_id] = cart_item.quantity
 
     return "OK"
@@ -70,5 +62,13 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
     print(f"{cart_id}: {cart_checkout}")
     customer_cart = cart_items.pop(cart_id)
+    with db.engine.begin() as connection:
+        connection.execute(
+            sqlalchemy.text(
+                "UPDATE global_inventory "
+                "SET num_red_potions = num_red_potions - :bottles, gold = gold + :gold"
+            )
+            .params(bottles=customer_cart, gold=customer_cart * 50)
+        )
 
     return {"total_potions_bought": customer_cart, "total_gold_paid": customer_cart * 50}
